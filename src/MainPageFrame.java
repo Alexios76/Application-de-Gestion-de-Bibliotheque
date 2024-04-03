844e5bd1bde5c0e7f490833288313a7a40616e71
hubertChavasse <hubchavasse@gmail.com>
2024-04-03 17:51:48 +0200

    modifs hubert

--- src/MainPageFrame.java
+++ src/MainPageFrame.java
@@ -3,40 +3,31 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MainPageFrame extends JFrame implements ActionListener {
-    // Constructeur de MainPageFrame
     public MainPageFrame() {
-        // Paramètres de la fenêtre principale
         setTitle("Système de Gestion de Bibliothèque");
         setSize(800, 600); // Dimension de la fenêtre
         setLocationRelativeTo(null); // Centrer la fenêtre
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // Barre de menu
         JMenuBar menuBar = new JMenuBar();
 
-        // Menu "Quitter"
         JMenuItem exitItem = new JMenuItem("Quitter");
         exitItem.addActionListener(this);
 
-        // Menu "Catalogue"
         JMenuItem catalogueItem = new JMenuItem("Catalogue");
         catalogueItem.addActionListener(this);
 
-        // Menu "Mes Livres"
         JMenuItem myBooksItem = new JMenuItem("Mes Livres");
         myBooksItem.addActionListener(this);
 
-        // Menu "Historique"
         JMenuItem historyItem = new JMenuItem("Historique");
         historyItem.addActionListener(this);
 
-        // Ajout des éléments de menu à la barre de menu
         menuBar.add(exitItem);
         menuBar.add(catalogueItem);
         menuBar.add(myBooksItem);
         menuBar.add(historyItem);
 
-        // Configuration de la barre de menu sur la fenêtre principale
         setJMenuBar(menuBar);
     }
 
@@ -46,18 +37,15 @@ public class MainPageFrame extends JFrame implements ActionListener {
 
         switch (actionCommand) {
             case "Quitter":
-                System.exit(0); // Fermer l'application
+                System.exit(0);
                 break;
             case "Catalogue":
-                // Afficher le panneau du catalogue ici
                 JOptionPane.showMessageDialog(this, "Coming soon");
                 break;
             case "Mes Livres":
-                // Afficher le panneau de "Mes Livres" ici
                 JOptionPane.showMessageDialog(this, "Coming soon");
                 break;
             case "Historique":
-                // Afficher le panneau d'historique ici
                 JOptionPane.showMessageDialog(this, "Coming soon");
                 break;
         }
